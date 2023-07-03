a57d86733ba8167c94b7b29c5e9d96fa42504b7b
Jennie Chen <jennie199911@gmail.com>
2023-07-02 23:43:16 -0700

    sign up and login modal added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,21 +1,20 @@
-import React from 'react';
+import React from "react";
 
-import './index.css';
+import "./index.css";
 
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { ModalProvider, Modal } from "./context/Modal";
+import App from "./App";
 
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-
-import configureStore from './store';
-
-import { restoreCSRF, csrfFetch } from './store/csrf';
+import configureStore from "./store";
+import { restoreCSRF, csrfFetch } from "./store/csrf";
 import * as sessionActions from "./store/session";
 
 const store = configureStore();
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
   restoreCSRF();
 
   window.csrfFetch = csrfFetch;
@@ -23,20 +22,23 @@ if (process.env.NODE_ENV !== 'production') {
   window.sessionActions = sessionActions;
 }
 
+
 function Root() {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-      <App />
-      </BrowserRouter>
-    </Provider>
+    <ModalProvider>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+          <Modal />
+        </BrowserRouter>
+      </Provider>
+    </ModalProvider>
   );
 }
-
 
 ReactDOM.render(
   <React.StrictMode>
     <Root />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -26,7 +26,7 @@ export const login = (user) => async(dispatch)=>{
           }),
     });
     const data = await res.json();
-    dispatch(setUser(data));
+    dispatch(setUser(data.user));
     return res;
 }
 
@@ -47,9 +47,20 @@ export const signup = (user) => async (dispatch) => {
     return response;
   };
 
+  export const logout = () => async (dispatch) => {
+    const response = await csrfFetch('/api/session', {
+      method: 'DELETE',
+    });
+    dispatch(removeUser());
+    return response;
+  };
+
 export const restoreUser = () => async (dispatch) => {
     const response = await csrfFetch("/api/session");
     const data = await response.json();
+    if(data.user==='null'){
+        data.user = null;
+    }
     dispatch(setUser(data.user));
     return response;
   };
@@ -57,10 +68,10 @@ export const restoreUser = () => async (dispatch) => {
 const initialState = { user: null };
 
  const sessionReducer = (state = initialState,action)=>{
-    let newState;
+
     switch(action.type){
         case SET_USER:
-            return {...state,user:action.payload.user};
+            return {...state,user:action.payload};
         case REMOVE_USER:
             return {...state,user:null};
         default:
